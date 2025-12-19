4561541c1597c0cc710c4779e2f78b991fd7d235
MainorC <Mainorlcumari@gmail.com>
2025-12-19 00:37:52 -0500

    Update Usuario.cs

--- Domain/Models/Usuario.cs
+++ Domain/Models/Usuario.cs
@@ -6,7 +6,7 @@ namespace RecursosHumanos.Domain.Models
         public string NombreUsuario { get; set; } = string.Empty;
         public string Contrasena { get; set; } = string.Empty;
         public string NombreCompleto { get; set; } = string.Empty;
-        public string Rol { get; set; } = "Empleado";
+        public string Rol { get; set; } = "Gerente";
         public bool Activo { get; set; } = true;
         public DateTime FechaCreacion { get; set; } = DateTime.Now;
     }
